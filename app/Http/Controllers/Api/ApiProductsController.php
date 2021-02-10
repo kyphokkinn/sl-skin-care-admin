cbf78159666f55f241f6677ed9e36140acb2ba86
Phokkinn KY <phokkinn.ky@gmail.com>
2021-02-10 23:48:59 +0700

    update search product title

--- app/Http/Controllers/Api/ApiProductsController.php
+++ app/Http/Controllers/Api/ApiProductsController.php
@@ -11,17 +11,22 @@
 				$this->table       = "tb_product";        
 				$this->permalink   = "products";    
 				$this->method_type = "get";    
+				$this->postdata = null;
 		    }
 		
 
 		    public function hook_before(&$postdata) {
 		        //This method will be execute before run the main process
-
+				CRUDBooster::copyPostdata($postdata, $this->postdata);
 		    }
 
 		    public function hook_query(&$query) {
 		        //This method is to customize the sql query
-
+				if (!empty($this->postdata['title'])) {
+					$query->whereRaw('title LIKE \'%'.$this->postdata['title'].'%\'');
+					unset($this->postdata['title']);
+				}
+				$query->where($this->postdata);
 		    }
 
 		    public function hook_after($postdata,&$result) {
