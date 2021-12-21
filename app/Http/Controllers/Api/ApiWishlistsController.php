c8c3e52931512635a3e5e5f4f34abcb26f9f5be5
bros chign <dev1@leapmohasambathlotto.com>
2021-12-21 22:40:47 +0700

    update wishlist api

--- app/Http/Controllers/Api/ApiWishlistsController.php
+++ app/Http/Controllers/Api/ApiWishlistsController.php
@@ -1,48 +1,60 @@
 <?php namespace App\Http\Controllers\Api;
 
-		use Session;
-		use Request;
-		use DB;
-		use CRUDBooster;
-use Doctrine\Common\Collections\Collection;
-use Illuminate\Support\Arr;
-
+use CRUDBooster;
+use DB;
 use function PHPSTORM_META\map;
 
-class ApiWishlistsController extends \crocodicstudio\crudbooster\controllers\ApiController {
+class ApiWishlistsController extends \crocodicstudio\crudbooster\controllers\ApiController
+{
 
-		    function __construct() {    
-				$this->table       = "tb_wishlist";        
-				$this->permalink   = "wishlists";    
-				$this->method_type = "get";    
-		    }
-		
+    function __construct()
+    {
+        $this->table = "tb_wishlist";
+        $this->permalink = "wishlists";
+        $this->method_type = "get";
+    }
 
-		    public function hook_before(&$postdata) {
-		        //This method will be execute before run the main process
+    public function hook_before(&$postdata)
+    {
+        //This method will be execute before run the main process
 
-		    }
+    }
 
-		    public function hook_query(&$query) {
-		        //This method is to customize the sql query
+    public function hook_query(&$query)
+    {
+        //This method is to customize the sql query
 
-		    }
+    }
 
-		    public function hook_after($postdata,&$result) {
-		        //This method will be execute after run the main process
-				if (!empty($postdata['created_by']) && count($result['data']) > 0) {
-			collect($result['data'])->map(function($item){
-					$item->product=DB::table('tb_product')
-					->where("id",$item->product_id)
-					->whereNull('deleted_at')
-					->first();
-					return $item;
-				});
-			
+    public function hook_after($postdata, &$result)
+    {
+        //This method will be execute after run the main process
+        if (!empty($postdata['created_by']) && count($result['data']) > 0) {
+            collect($result['data'])->map(function ($item) {
+                if ($item->product_id != null) {
+                    $item->product = DB::table('tb_product')
+                        ->where("id", $item->product_id)
+                        ->whereNull('deleted_at')
+                        ->first();
+                    if ($item->product->image_path) {
+                        $item->product->image_path = url($item->product->image_path);
+                    }
+                }
+                if ($item->promotion_id != null) {
+                    $item->promotion = DB::table('tb_promotion')
+                        ->where("id", $item->promotion_id)
+                        ->whereNull('deleted_at')
+                        ->first();
+                    if ($item->promotion->image_path) {
+                        $item->promotion->image_path = url($item->promotion->image_path);
+                    }
+                }
 
-				}
+                return $item;
+            });
 
+        }
 
-		    }
+    }
 
-		}
+}
