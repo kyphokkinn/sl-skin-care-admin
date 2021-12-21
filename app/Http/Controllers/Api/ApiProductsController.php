6a8024e7db2948d35a247396c663882ed29ff964
bros chign <dev1@leapmohasambathlotto.com>
2021-12-21 23:10:49 +0700

    get product with wishlist

--- app/Http/Controllers/Api/ApiProductsController.php
+++ app/Http/Controllers/Api/ApiProductsController.php
@@ -1,90 +1,109 @@
 <?php namespace App\Http\Controllers\Api;
 
-		use Session;
-		use Request;
-		use DB;
-		use CRUDBooster;
+use CRUDBooster;
+use DB;
+use Request;
 
-		class ApiProductsController extends \crocodicstudio\crudbooster\controllers\ApiController {
+class ApiProductsController extends \crocodicstudio\crudbooster\controllers\ApiController
+{
 
-		    function __construct() {    
-				$this->table       = "tb_product";        
-				$this->permalink   = "products";    
-				$this->method_type = "get";    
-				$this->postdata = null;
-		    }
-		
+    public function __construct()
+    {
+        $this->table = "tb_product";
+        $this->permalink = "products";
+        $this->method_type = "get";
+        $this->postdata = null;
+        $this->user_id = null;
+    }
 
-		    public function hook_before(&$postdata) {
-		        //This method will be execute before run the main process
-				CRUDBooster::copyPostdata($postdata, $this->postdata);
-		    }
+    public function hook_before(&$postdata)
+    {
+        //This method will be execute before run the main process
+        CRUDBooster::copyPostdata($postdata, $this->postdata);
+        $this->user_id = $postdata['user_id'];
+        unset($postdata['user_id'], $this->postdata['user_id']);
+    }
 
-		    public function hook_query(&$query) {
-		        //This method is to customize the sql query
-				if (!empty($this->postdata['title'])) {
-					$query->whereRaw('title LIKE \'%'.$this->postdata['title'].'%\'');
-					unset($this->postdata['title']);
-				}
-				if ($this->postdata != null) {
-					$query->where($this->postdata);
-				}
-		    }
+    public function hook_query(&$query)
+    {
+        //This method is to customize the sql query
+        if (!empty($this->postdata['title'])) {
+            $query->whereRaw('title LIKE \'%' . $this->postdata['title'] . '%\'');
+            unset($this->postdata['title']);
+        }
+        if ($this->postdata != null) {
+            $query->where($this->postdata);
+        }
+    }
 
-		    public function hook_after($postdata,&$result) {
-		        //This method will be execute after run the main process
-				if (!empty($result['data'])) {
-					foreach($result['data'] as $item) {
-						$item->price_config = DB::table('tb_price_config')
-							->selectRaw('id,product_id,from_item,to_item,price')
-							->whereNull('deleted_at')
-							->where('product_id', $item->id)
-							->where('status', 1)
-							->get();
-						$item->selected_price = $item->price;
-						if ($item->price_on_sale) {
-							$item->selected_price = $item->price_on_sale;
-							if (!(empty($item->on_sale_start) || empty($item->on_sale_end))) {
-								$current = date('Y-m-d');
-								$current = date('Y-m-d', strtotime($current));
-								$start_date = date('Y-m-d', strtotime($item->on_sale_start));
-								$end_date = date('Y-m-d', strtotime($item->on_sale_end));
-								if (($current >= $start_date) && ($current <= $end_date)){
-									$item->selected_price = $item->price_on_sale;
-								} else {
-									$item->selected_price = $item->price;
-								}
-							}
-						}
-						if ($item->image_path) {
-						       $item->image_path = url($item->image_path);
-							     }
-					}
-				}
-		    }
-			
-			public function get_random() {
-				    $params = Request::all();
-				    $result = array('api_status' => 1, 'api_message' => 'success', 'data' => null);
-				    $where = [];
-				    $where[] = '1=1';
-				    if(isset($params['id'])) {
-				     $where[] = 'id NOT IN ('.$params['id'].')';
-				    }
-				    $data = DB::table('tb_product')
-				     ->whereNull('deleted_at')
-				     ->whereRaw(implode(' AND ', $where))
-				     ->limit(10)
-				     ->get();
-				    if($data->count() == 0) {
-				     $data = DB::table('tb_product')
-				      ->whereNull('deleted_at')
-				      ->limit(10)
-				      ->get();
-				    }
-				    $result['data'] = $data;
-				    $this->hook_after($params, $result);
-				    return response()->json($result);
-				    
-				   }
-		}
+    public function hook_after($postdata, &$result)
+    {
+        //This method will be execute after run the main process
+        if (!empty($result['data'])) {
+            foreach ($result['data'] as $item) {
+                $item->price_config = DB::table('tb_price_config')
+                    ->selectRaw('id,product_id,from_item,to_item,price')
+                    ->whereNull('deleted_at')
+                    ->where('product_id', $item->id)
+                    ->where('status', 1)
+                    ->get();
+
+                $wishlist = DB::table('tb_wishlist')
+                    ->whereNull('deleted_at')
+                    ->where('created_by', $postdata->user_id)
+                    ->where('product_id', $item->id)
+                    ->first();
+                if ($wishlist) {
+                    $item->wishlist_id = $wishlist->id;
+                } else {
+                    $item->wishlist_id = null;
+                }
+
+                $item->selected_price = $item->price;
+                if ($item->price_on_sale) {
+                    $item->selected_price = $item->price_on_sale;
+                    if (!(empty($item->on_sale_start) || empty($item->on_sale_end))) {
+                        $current = date('Y-m-d');
+                        $current = date('Y-m-d', strtotime($current));
+                        $start_date = date('Y-m-d', strtotime($item->on_sale_start));
+                        $end_date = date('Y-m-d', strtotime($item->on_sale_end));
+                        if (($current >= $start_date) && ($current <= $end_date)) {
+                            $item->selected_price = $item->price_on_sale;
+                        } else {
+                            $item->selected_price = $item->price;
+                        }
+                    }
+                }
+                if ($item->image_path) {
+                    $item->image_path = url($item->image_path);
+                }
+            }
+        }
+    }
+
+    public function get_random()
+    {
+        $params = Request::all();
+        $result = array('api_status' => 1, 'api_message' => 'success', 'data' => null);
+        $where = [];
+        $where[] = '1=1';
+        if (isset($params['id'])) {
+            $where[] = 'id NOT IN (' . $params['id'] . ')';
+        }
+        $data = DB::table('tb_product')
+            ->whereNull('deleted_at')
+            ->whereRaw(implode(' AND ', $where))
+            ->limit(10)
+            ->get();
+        if ($data->count() == 0) {
+            $data = DB::table('tb_product')
+                ->whereNull('deleted_at')
+                ->limit(10)
+                ->get();
+        }
+        $result['data'] = $data;
+        $this->hook_after($params, $result);
+        return response()->json($result);
+
+    }
+}
